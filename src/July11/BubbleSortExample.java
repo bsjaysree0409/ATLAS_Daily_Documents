9f1162a82dd1969375277073d0015fb541f396b9
bsjaysree0409 <jaysreesangeetha@gmail.com>
2025-07-11 11:45:26 +0530

    BubbleSortExample

---
+++ src/July11/BubbleSortExample.java
@@ -0,0 +1,55 @@
+package July11;
+
+public class BubbleSortExample {
+
+    // Method to perform bubble sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        // Outer loop for passes
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            // Inner loop for comparing adjacent elements
+            for (int j = 0; j < n - i - 1; j++) {
+                // If elements are in the wrong order, swap them
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            // If no two elements were swapped in the inner loop, list is sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    // Method to print the array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        int[] myList = {45, 12, 89, 33, 25};
+
+        System.out.println("Original array:");
+        printArray(myList);
+
+        // Sort the array using bubble sort
+        bubbleSort(myList);
+
+        System.out.println("Sorted array:");
+        printArray(myList);
+    }
+}
